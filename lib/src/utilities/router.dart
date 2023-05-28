f8a7e5b3609ae4c1cee0ed1fb3bd8fdf9fe890d0
Candra Ramesthy <candraramadhanp@gmail.com>
2023-05-28 12:25:46 +0800

    add HomeScreen

---
+++ lib/src/utilities/router.dart
@@ -0,0 +1,10 @@
+part of 'utilities.dart';
+
+final GoRouter router = GoRouter(routes: [
+  GoRoute(
+    path: '/',
+    builder: (context, state) {
+      return //TODO create Home Widget
+    },
+  ),
+]);
